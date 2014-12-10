84273d596de569f8cd53dfca219186d1350a3bc7
Anatoly Bubenkov <bubenkoff@gmail.com>
2014-12-10 17:53:54 +0100

    correct settings handling

--- manage.py
+++ manage.py
@@ -7,11 +7,15 @@ import gae2django
 # admin frontend at the top of each page. By default this link is hidden.
 gae2django.install(server_software='Django')
 
-# apply patches
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paylogic.settings_paylogic")
-import paylogic.patches  # NOQA
-
 from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
+    # apply patches
+    if '--settings=paylogic.settings_build' in sys.argv:
+        settings = 'paylogic.settings_build'
+    else:
+        settings = 'paylogic.settings_paylogic'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+    import paylogic.patches  # NOQA
+
     execute_from_command_line(sys.argv)
