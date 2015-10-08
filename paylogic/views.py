249af678fc1b239dbc767d545f8b3af05707bf2e
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-10-08 17:19:08 +0200

    strip tags

--- paylogic/views.py
+++ paylogic/views.py
@@ -373,9 +373,9 @@ def get_fogbugz_tags(request, case_number=None):
         resp = fogbugz_instance.search(q=case_number, cols='tags')
     else:
         resp = fogbugz_instance.listTags()
-    return [
+    return sorted(
         tag for tag in (tg.find('stag').text.strip() for tg in resp.findAll('tag'))
-        if request.REQUEST.get('term', '').lower() in tag.lower()]
+        if request.REQUEST.get('term', '').lower() in tag.lower())
 
 
 def get_issue(request, case_number, case_title):
